9db9af48292aa16ba862d36dd1fe13e7d5a5cbf2
deretame <81013544+deretame@users.noreply.github.com>
2023-08-20 04:41:25 -0400

    ✨ feat:

---
+++ Electronic_product_mass_production_tools/12_input_manager_unittest/include/input_manager.h
@@ -0,0 +1,36 @@
+#ifndef _INPUT_MANAGER_H_
+#define _INPUT_MANAGER_H_
+
+#include <sys/time.h>
+
+#define INPUT_TYPE_TOUCH 1
+#define INPUT_TYPE_NET   2
+#define TOUCHSCREEN      0
+#define NETTINPUT        1
+
+struct timeval;
+
+typedef struct InputEvent
+{
+    struct timeval tTime;
+    int iType;
+    int iX;
+    int iY;
+    int iPressure;
+    char str[1024];
+} InputEvent, *pInputEvent;
+
+typedef struct InputDevice
+{
+    char * name;
+    int (*GetInputEvent)(pInputEvent ptInputEvent);
+    int (*DeviceInit)(void);
+    int (*DeviceExit)(void);
+    struct InputDevice * ptNext;
+} InputDevice, *pInputDevice;
+
+void RegisterInputDevice(pInputDevice ptInputOpr);
+
+void InputInit(void);
+
+#endif   // !_INPUT_MANAGER_H_
