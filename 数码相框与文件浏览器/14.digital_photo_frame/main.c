b52bc9ce130bd40c005518e3a87d0d84870cc624
deretame <81013544+deretame@users.noreply.github.com>
2023-09-13 08:30:07 -0400

    🎉 init:

---
+++ 数码相框与文件浏览器/14.digital_photo_frame/main.c
@@ -0,0 +1,203 @@
+#include "include/config.h"
+#include "include/disp_manager.h"
+#include "include/draw.h"
+#include "include/encoding_manager.h"
+#include "include/fonts_manager.h"
+#include "include/input_manager.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/* ./show_file [-s Size] [-f freetype_font_file] [-h HZK] <text_file> */
+int main(int argc, char ** argv)
+{
+    int iError;
+    unsigned int dwFontSize = 16;
+    char acHzkFile[128];
+    char acFreetypeFile[128];
+    char acTextFile[128];
+
+    char acDisplay[128];
+
+    int bList = 0;
+
+    T_InputEvent tInputEvent;
+
+    acHzkFile[0]      = '\0';
+    acFreetypeFile[0] = '\0';
+    acTextFile[0]     = '\0';
+
+    strcpy(acDisplay, "fb");
+
+    iError = DebugInit();
+    if (iError)
+    {
+        DebugPrint("DebugInit error!\n");
+        return -1;
+    }
+
+    InitDebugChanel();
+
+    while ((iError = getopt(argc, argv, "ls:f:h:d:")) != -1)
+    {
+        switch (iError)
+        {
+        case 'l':
+        {
+            bList = 1;
+            break;
+        }
+        case 's':
+        {
+            dwFontSize = strtoul(optarg, NULL, 0);
+            break;
+        }
+        case 'f':
+        {
+            strncpy(acFreetypeFile, optarg, 128);
+            acFreetypeFile[127] = '\0';
+            break;
+        }
+        case 'h':
+        {
+            strncpy(acHzkFile, optarg, 128);
+            acHzkFile[127] = '\0';
+            break;
+        }
+        case 'd':
+        {
+            strncpy(acDisplay, optarg, 128);
+            acDisplay[127] = '\0';
+            break;
+        }
+        default:
+        {
+            DebugPrint("Usage: %s [-s Size] [-d display] [-f font_file] [-h HZK] <text_file>\n", argv[0]);
+            DebugPrint("Usage: %s -l\n", argv[0]);
+            return -1;
+            break;
+        }
+        }
+    }
+
+    if (!bList && (optind >= argc))
+    {
+        DebugPrint("Usage: %s [-s Size] [-d display] [-f font_file] [-h HZK] <text_file>\n", argv[0]);
+        DebugPrint("Usage: %s -l\n", argv[0]);
+        return -1;
+    }
+
+    iError = DisplayInit();
+    if (iError)
+    {
+        DebugPrint("DisplayInit error!\n");
+        return -1;
+    }
+
+    iError = FontsInit();
+    if (iError)
+    {
+        DebugPrint("FontsInit error!\n");
+        return -1;
+    }
+
+    iError = EncodingInit();
+    if (iError)
+    {
+        DebugPrint("EncodingInit error!\n");
+        return -1;
+    }
+
+    iError = InputInit();
+    if (iError)
+    {
+        DebugPrint("InputInit error!\n");
+        return -1;
+    }
+
+    if (bList)
+    {
+        DebugPrint("supported display:\n");
+        ShowDispOpr();
+
+        DebugPrint("supported font:\n");
+        ShowFontOpr();
+
+        DebugPrint("supported encoding:\n");
+        ShowEncodingOpr();
+
+        DebugPrint("supported input:\n");
+        ShowInputOpr();
+
+        DebugPrint("supported debug chanel:\n");
+        ShowDebugOpr();
+
+        return 0;
+    }
+
+    strncpy(acTextFile, argv[optind], 128);
+    acTextFile[127] = '\0';
+
+    iError = OpenTextFile(acTextFile);
+    if (iError)
+    {
+        DebugPrint("OpenTextFile error!\n");
+        return -1;
+    }
+
+    iError = SetTextDetail(acHzkFile, acFreetypeFile, dwFontSize);
+    if (iError)
+    {
+        DebugPrint("SetTextDetail error!\n");
+        return -1;
+    }
+
+    DebugPrint("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+
+    iError = SelectAndInitDisplay(acDisplay);
+    if (iError)
+    {
+        DebugPrint("SelectAndInitDisplay error!\n");
+        return -1;
+    }
+
+    iError = AllInputDevicesInit();
+    if (iError)
+    {
+        DebugPrint("Error AllInputDevicesInit\n");
+        return -1;
+    }
+
+    DebugPrint("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+    iError = ShowNextPage();
+    DebugPrint("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+    if (iError)
+    {
+        DebugPrint("Error to show first page\n");
+        return -1;
+    }
+
+    DebugPrint("Enter 'n' to show next page, 'u' to show previous page, 'q' to exit: ");
+
+    while (1)
+    {
+
+        if (0 == GetInputEvent(&tInputEvent))
+        {
+            if (tInputEvent.iVal == INPUT_VALUE_DOWN)
+            {
+                ShowNextPage();
+            }
+            else if (tInputEvent.iVal == INPUT_VALUE_UP)
+            {
+                ShowPrePage();
+            }
+            else if (tInputEvent.iVal == INPUT_VALUE_EXIT)
+            {
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
